79beb00fdfa83cd847edbeb12c737649e4363397
Kazuhisa Hara <kazuhisya@gmail.com>
2017-01-15 00:20:00 +0900

    misc

--- updater.sh
+++ updater.sh
@@ -4,12 +4,9 @@ env_file="./envfile.txt"
 pj_file="./projects.txt"
 
 env=(`cat ${env_file}`)
-#echo ${env[@]}
-
 numLine=1
 cat ${pj_file} | while read pj_name
 do
-	#echo $numLine: $pj_name
 	echo "circlecli env -p ${pj_name} ${env[@]}"
 	circlecli env -p ${pj_name} ${env[@]}
 	numLine=$((numLine + 1))
